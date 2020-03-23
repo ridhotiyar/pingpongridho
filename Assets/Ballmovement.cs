ec17fb800631f1489deea7406dd9e322393bade4
ridhotiyar <61532462+ridhotiyar@users.noreply.github.com>
2020-03-23 12:45:16 +0700

    reference

---
+++ Assets/Ballmovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ballmovement : MonoBehaviour
+{
+    public int speed = 30;
+
+    public Rigidbody2D ball;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        ball.velocity = new Vector2(-1,-1)*speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if(other.collider.name=="Kanan" || other.collider.name=="Kiri")
+        {
+            GetComponent<Transform>().position = new Vector2(0,0);
+        }
+    }
+}
