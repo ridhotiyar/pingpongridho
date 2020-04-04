23b81f9c8a90416aebb20c781946fe58a5e3617f
ridhotiyar <61532462+ridhotiyar@users.noreply.github.com>
2020-04-04 15:36:55 +0800

    fire_ball

--- Assets/Ballmovement.cs
+++ Assets/Ballmovement.cs
@@ -8,16 +8,25 @@ public class Ballmovement : MonoBehaviour
 
     public Rigidbody2D ball;
     
+    public Animator anim;
     // Start is called before the first frame update
     void Start()
     {
         ball.velocity = new Vector2(-1,-1)*speed;
+        anim.SetBool("isMove", true);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(ball.velocity.x>0)
+        {
+            ball.GetComponent<Transform>().localScale = new Vector3(1, 1, 1);
+        }
+        else
+        {
+            ball.GetComponent<Transform>().localScale = new Vector3(-1, 1, 1);
+        }
     }
 
     void OnCollisionEnter2D(Collision2D other)
@@ -31,8 +40,10 @@ public class Ballmovement : MonoBehaviour
     IEnumerator jeda()
     {
         ball.velocity = Vector2.zero;
+        anim.SetBool("isMove", false);
         ball.GetComponent<Transform>().position = Vector2.zero;
         yield return new WaitForSeconds(1);
         ball.velocity = new Vector2(-1,-1) * speed;
+        anim.SetBool("isMove", true);
     }
 }
